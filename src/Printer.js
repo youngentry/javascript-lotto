60e3b2399cf29c270c647dc85ddc69dbebf5d952
young <gentry_@naver.com>
2022-11-13 13:15:09 +0900

    feat: 메시지를 출력하기 위한 Printer Class 생성

---
+++ src/Printer.js
@@ -0,0 +1,48 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("./constant/message");
+
+class Printer {
+    constructor() {}
+
+    purchase() {
+        return Console.print(MESSAGE.PURCHASE);
+    }
+
+    purchaseAmount(amount) {
+        return Console.print(amount + MESSAGE.SHOW_PURCHASE);
+    }
+
+    sixNumbers(sixNumberArray) {
+        return sixNumberArray.forEach((sixNumber) => {
+            Console.print(sixNumber);
+        });
+    }
+
+    inputLottoNumber(input) {
+        Console.print(MESSAGE.LOTTO_NUMBER);
+        return Console.print(input);
+    }
+
+    inputBonusNumber(input) {
+        Console.print(MESSAGE.BONUS_NUMBER);
+        return Console.print(input);
+    }
+
+    lottoResult(winnings) {
+        for (let i = 0; i < winnings.length; i++) {
+            Console.print(MESSAGE.WINNING[`${i}`] + winnings[i] + MESSAGE.KOREAN_AMOUNT_WORD);
+        }
+    }
+}
+// const print = new Printer();
+// print.purchase();
+// print.purchaseAmount(2);
+// print.sixNumbers([
+//     [1, 2, 3, 4, 5, 6],
+//     [2, 3, 4, 5, 6, 7],
+// ]);
+// print.inputLottoNumber("1,2,3,4,5,6");
+// print.inputBonusNumber(7);
+// print.lottoResult([1, 0, 0, 0, 1]);
+
+module.exports = Printer;
---
+++ src/constant/message.js
@@ -0,0 +1,19 @@
+const MESSAGE = {
+    PURCHASE: "구입금액을 입력해 주세요.",
+    SHOW_PURCHASE: "개를 구매했습니다.",
+    LOTTO_NUMBER: "당첨 번호를 입력해 주세요.",
+    BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
+    RESULT: "당첨 통계" + "\n" + "---",
+    WINNING: {
+        0: "3개 일치 (5,000원) - ",
+        1: "4개 일치 (50,000원) - ",
+        2: "5개 일치 (1,500,000원) - ",
+        3: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+        4: "6개 일치 (2,000,000,000원) - ",
+    },
+    YIELD: "총 수익률은",
+    KOREAN_ENDING_WORD: "입니다.",
+    KOREAN_AMOUNT_WORD: "개",
+};
+
+module.exports = { MESSAGE };
