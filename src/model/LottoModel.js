dffea44ec4cbe8406f1ca9c34d316d32568855e8
youngentry <90388461+youngentry@users.noreply.github.com>
2022-12-15 23:21:48 +0900

    feat(LottoModel): revenue 필드 추가

--- src/model/LottoModel.js
+++ src/model/LottoModel.js
@@ -3,36 +3,54 @@ class LottoModel {
     #bonusNumber;
     #winnings;
     #prize;
+    #revenue;
 
     setMoney(money) {
         this.#money = money;
     }
+
     getMoney() {
         return this.#money;
     }
+
     setBonusNumber(bonusNumber) {
         this.#bonusNumber = bonusNumber;
     }
+
     getBonusNumber() {
         return this.#bonusNumber;
     }
+
     setBonusNumber(bonusNumber) {
         this.#bonusNumber = bonusNumber;
     }
+
     getBonusNumber() {
         return this.#bonusNumber;
     }
+
     setWinnings(winnings) {
         this.#winnings = winnings;
     }
+
     getWinnings() {
         return this.#winnings;
     }
+
     setPrize(prize) {
         this.#prize = prize;
     }
+
     getPrize() {
         return this.#prize;
+    }
+
+    setRevenue(revenue) {
+        this.#revenue = revenue;
+    }
+
+    getRevenue() {
+        return this.#revenue;
     }
 }
 
